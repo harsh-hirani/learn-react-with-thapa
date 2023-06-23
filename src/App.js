45e57f47e936a98ad62362a8438260394c103623
Harsh Hirani <hiraniharsh7@gmail.com>
2023-06-23 17:42:13 +0530

    #30 useState counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";  
 
 function App() {
+  const [count,setCount] = useState(0);
+  let sum = (a) => {setCount(count + 1);}
+  let down = (a) => {setCount(count - 1);}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Count: {count}</p>
+      <br />
+      <button onClick={sum}>plus</button>
+      <button onClick={down}>down</button>
+    </>
   );
 }
 
